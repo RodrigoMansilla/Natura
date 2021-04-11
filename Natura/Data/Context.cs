846673491d241bffec49c4567ddf4921b6223a17
Rodrigo Mansilla <rodrigoomansilla@hotmail.com>
2021-04-11 06:26:15 -0300

    Agregar archivos de proyecto.

---
+++ Natura/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Natura.Data
+{
+    public class Context: DbContext
+    {
+        public Context() : base("DefaultConnection")
+        {
+
+        }
+       // public DbSet<IngresoGasto> IngresosGastos { get; set; }
+    }
+}
---
+++ Natura/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Natura.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int IdProducto { get; set; }
+
+        //[Required]
+        //[StringLength(100, MinimumLength = 3)]
+        //public string Nombre { get; set; }
+
+        //[Required]
+        //[StringLength(50, MinimumLength = 3)]
+        //public string Categoria { get; set; }
+
+        //[Required]
+        //public int Codigo { get; set; }
+
+
+    }
+}
